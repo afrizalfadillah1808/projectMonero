780087f66f9d39da4dd342d7d49c6bc01f1ffa52
unknown <muhammadafrizalfadillah@gmail.com>
2022-12-25 17:25:44 +0700

    Halaman Class

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+    <link rel="" href="css/carousel.css">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Monero Course | {{ $title }}</title>
 </head>
 <body>
