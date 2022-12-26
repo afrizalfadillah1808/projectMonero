c17efa33ba8a0d5e15d4c9999bed59d56862caab
widyn84 <widyn84@gmail.com>
2022-12-26 22:57:11 +0700

    Back End Home Review

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view ('home', [
         "title"=>"Home",
-        "active"=>'home'
+        "active"=>'home',
+        "user" => User::all()
     ]);
 });
 
