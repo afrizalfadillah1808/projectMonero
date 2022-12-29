2548d6ce58e80c4d222aa0ea369906408e88d449
unknown <muhammadafrizalfadillah@gmail.com>
2022-12-29 22:53:54 +0700

    revisi-kelas

--- resources/views/class.blade.php
+++ resources/views/class.blade.php
@@ -1,111 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="first-class-section w-100">
-  <div class="top-section d-flex flex-column p-5 align-items-center">
-    <h3 class="text-indigo-900 fw-bold">Mau <span class="text-indigo-600">Belajar</span> Apa Kita <span class="text-indigo-600">Hari Ini</span> ?</h3>
-    <p class="text-indigo-900">Ayo Tingkatkan Skillmu Bersama Monero Course</p>
-    <div class="search-class w-25">
-      <div class="input-group rounded-xl shadow-sm">
-        <input type="text" class="border-0 px-3 rounded-xl form-control" placeholder="Search..." style="outline: none; box-shadow: none; padding-top: 12px; padding-bottom: 12px;">
-        <button class="border-0 bg-indigo-600 rounded-xl px-3" type="button"><i class="fa-solid fa-search text-indigo-100"></i></button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<h3 class="text-indigo-600 fw-bold px-5 mx-3 mb-3">Kupon <span class="text-indigo-900">Diskon</span></h3>
-<div class="diskon-section d-flex flex-column gap-3 mb-5 px-5 mx-3">
-  <div class="diskon-list d-flex flex-row gap-3">
-    <div class="discount-1 p-3 d-flex flex-column gap-3 shadow-sm rounded-xl bg-indigo-600 text-indigo-100" style="width: 25%">
-      <div class="discount-content">
-        <p class="fw-medium" style="font-size: 18px; font-style: italic;">Diskon 60.000</p>
-        <p>Diskon ini terbatas. Gunakan Diskon kamu sekarang sebelum kehabisan.</p>
-      </div>
-      <hr class="text-indigo-100 border-2 my-0">
-      <div class="discount-bottom my-auto d-flex flex-row justify-content-between">
-        <div class="left-bottom gap-3 d-flex flex-row align-items-center">
-          <i class="fa-solid fa-clock fs-4"></i>
-          <p class="my-auto fw-medium">3 Jam, 30 Menit</p>
-        </div>
-        <div class="right-bottom">
-          <button class="fw-medium border-0 bg-indigo-100 text-indigo-600 px-4 py-2 shadow shadow-sm rounded-xl copy-code">Salin Kode</button>
-        </div>
-      </div>
-    </div>
-    <div class="discount-2 p-3 d-flex flex-column gap-3 shadow-sm rounded-xl bg-indigo-600 text-indigo-100" style="width: 25%">
-      <div class="discount-content">
-        <p class="fw-medium" style="font-size: 18px; font-style: italic;">Diskon 60.000</p>
-        <p>Diskon ini terbatas. Gunakan Diskon kamu sekarang sebelum kehabisan.</p>
-      </div>
-      <hr class="text-indigo-100 border-2 my-0">
-      <div class="discount-bottom my-auto d-flex flex-row justify-content-between">
-        <div class="left-bottom gap-3 d-flex flex-row align-items-center">
-          <i class="fa-solid fa-clock fs-4"></i>
-          <p class="my-auto fw-medium">3 Jam, 30 Menit</p>
-        </div>
-        <div class="right-bottom">
-          <button class="fw-medium border-0 bg-indigo-100 text-indigo-600 px-4 py-2 shadow shadow-sm rounded-xl copy-code">Salin Kode</button>
-        </div>
-      </div>
-    </div>
-    <div class="discount-3 p-3 d-flex flex-column gap-3 shadow-sm rounded-xl bg-indigo-600 text-indigo-100" style="width: 25%">
-      <div class="discount-content">
-        <p class="fw-medium" style="font-size: 18px; font-style: italic;">Diskon 60.000</p>
-        <p>Diskon ini terbatas. Gunakan Diskon kamu sekarang sebelum kehabisan.</p>
-      </div>
-      <hr class="text-indigo-100 border-2 my-0">
-      <div class="discount-bottom my-auto d-flex flex-row justify-content-between">
-        <div class="left-bottom gap-3 d-flex flex-row align-items-center">
-          <i class="fa-solid fa-clock fs-4"></i>
-          <p class="my-auto fw-medium">3 Jam, 30 Menit</p>
-        </div>
-        <div class="right-bottom">
-          <button class="fw-medium border-0 bg-indigo-100 text-indigo-600 px-4 py-2 shadow shadow-sm rounded-xl copy-code">Salin Kode</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-  <div id="liveToast" class="toast hide bg-indigo-115 shadow-sm rounded-xl" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header bg-indigo-600" style="border-top-left-radius: 0.75rem; border-top-right-radius: 0.75rem;">
-      <strong class="me-auto text-indigo-100">Diskon</strong>
-      <small class="text-indigo-100">0 Mins Ago</small>
-      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-    <div class="toast-body fw-medium text-indigo-900">Kode Diskon Berhasil Disalin!</div>
-  </div>
-</div>
-
-<h3 class="text-indigo-900 fw-bold px-5 mx-3 mb-3">Kelas di <span class="text-indigo-600">Monero Course</span></h3>
-<div class="list-class-section d-flex align-content-start flex-wrap gap-3 pb-5 px-5 mx-3 justify-content-center justify-content-lg-start">
-  @foreach ($classSubjects as $cS)
-  <a href="/class/{{ $cS->slugCourse }}" class="text-decoration-none">
-    <div class="first-class bg-indigo-600 min-h-100 rounded-xl d-flex flex-column shadow-sm gap-3 p-3" style="width: 330px; min-height: auto">
-      <img src="https://source.unsplash.com/330x200?Web+Programming" class="img-fluid rounded-xl" alt="" style="width: 330px; max-height: 200px;">
-      <div class="first-class-section gap-0">
-        <p class="my-auto fw-medium text-indigo-100" style="font-size: 18px; font-style: italic;">{{ $cS->namaCourse }}</p>
-        <p class="text-indigo-100">{{ $cS->category->namaCategory }}</p>
-        <p class="text-indigo-100">
-          {{ $cS->lamaVideo }} Belajar dengan {{ $cS->jumlahVideo }}
-        </p>
-        <div class="first-class-section-mentor d-flex gap-3">
-          <img src="https://source.unsplash.com/100x100?Person" class="img-fluid rounded-circle" alt="" style="max-width: 49.9px; max-height: 49.9px;">
-          <div class="first-class-section-mentor-content d-flex flex-column">
-            <p class="my-auto text-indigo-100">{{ $cS->mentor->namaMentor }}</p>
-            <p class="my-auto fw-medium text-indigo-100">Mentors</p>
-          </div>
-        </div>
-      </div>
-      <hr class="text-indigo-100 border-2 my-0">
-      <div class="second-class-section-bottom d-flex align-items-center justify-content-between">
-        <p class="my-auto text-indigo-100 fw-medium">{{ $cS->hargaCourse }}</p>
-        <p class="my-auto text-indigo-100 fw-medium"><i class="fa-solid fa-star text-indigo-100 fs-5"></i> 4.8</p>
-      </div>
-    </div>
-  </a>
-  @endforeach
+<h3 class="text-indigo-600 fw-bold pt-5 px-5 mx-3 mb-3">Kelas <span class="text-indigo-900">{{ $class->namaCourse }}</span></h3>
+<div class="class-details d-flex flex-column text-indigo-900 px-5 mx-3">
+  <p>Mentored by {{ $kelas->mentor->namaMentor }} | <a class="text-indigo-600" href="/categories/{{ $kelas->category->slugCategory }}" style="text-decoration: none;">{{ $class->category->namaCategory }}</a></p>
+  <p>{{ $kelas->lamaVideo }} Belajar dengan <span> {{ $kelas->jumlahVideo }}</span></p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta repellendus consequatur debitis, in cumque dignissimos pariatur at corporis non adipisci quo unde eum voluptate cum nam, deleniti iure magnam placeat quod earum facilis! Possimus reprehenderit aliquid, modi facilis doloremque quibusdam at consequuntur maxime pariatur obcaecati incidunt porro in sit laborum mollitia ea totam accusamus aut. Nihil facere earum, dignissimos quasi eos exercitationem possimus itaque laborum aspernatur architecto aperiam numquam voluptate et dolorum soluta laudantium! Laboriosam eaque amet expedita perspiciatis dolor placeat minima deleniti quas quia nisi reprehenderit, temporibus, a nesciunt quae omnis facere laudantium eius! Quas temporibus a aperiam quae magni autem accusantium ea suscipit ex unde. Maxime eius eaque quas labore quia porro temporibus ipsam tempore, sapiente ullam eveniet dicta quibusdam saepe? Praesentium, earum expedita autem quaerat totam maxime! Ea, delectus sed repudiandae veniam quia asperiores ex? Et rem nisi maiores inventore soluta consequatur, quisquam, eligendi placeat culpa expedita, accusantium nihil facilis harum illo quibusdam dolor. Dolorum non fugit enim maxime excepturi mollitia consectetur doloribus quibusdam quod voluptatibus molestias corrupti neque atque commodi praesentium veniam aut ab sed cum, tempore fuga maiores repudiandae! Itaque corporis ad ex necessitatibus animi dignissimos eos inventore facere! Facilis sunt ducimus quod porro magnam.</p>
+  <p>{{ $kelas->hargaCourse }}</p>
+  <a href="/classes" class="text-indigo-600" style="text-decoration: none;">Back to Classes</a>
 </div>
 @endsection
